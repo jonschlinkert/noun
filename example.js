2cbbc1671eca45ee5768da3028f4ccaaf8d237ef
jonschlinkert <dev@sellside.com>
2014-10-22 01:48:50 -0400

    update the examples

--- example.js
+++ example.js
@@ -1,21 +1,69 @@
+'use strict';
+
+var util = require('util');
 var Noun = require('./');
-var noun = new Noun();
 
-noun
-  .use(function() {
-    var pkg = this.get('one');
-    console.log(pkg)
-    this.context.one = this.context.foo;
-  })
-  .use(function() {
-    this.context.bbb = this.context.bar;
-  })
-  .use(baz({what: 'whhhhhaaaa?'}))
+/**
+ * The readem shows how to create an instance of Noun,
+ * this shows how to inherit `Noun`.
+ */
 
-function baz(options) {
-  return function () {
-    this.set('one', 'two');
-    this.context.ccc = this.context.baz;
+function App(options) {
+  Noun.call(this, 'app');
+  this.context = {};
+}
+util.inherits(App, Noun);
+
+/**
+ * Add some methods to our app
+ */
+
+App.prototype.set = function(key, value) {
+  this.context[key] = value;
+  return this;
+};
+
+App.prototype.get = function(key) {
+  return this.context[key];
+};
+
+
+/**
+ * Usage
+ *
+ * Create an instance of `App`
+ */
+
+var app = new App();
+
+/**
+ * Define some plugins
+ */
+
+function foo(options) {
+  return function (foo) {
+    this.set('foo', options);
   }
 }
-// console.log(noun);
+function bar(options) {
+  return function (foo) {
+    this.set('bar', options);
+  }
+}
+function baz(options) {
+  return function (foo) {
+    this.set('baz', options);
+  }
+}
+
+/**
+ * Run the plugins!
+ */
+
+app
+  .plugin(foo({a: 'b'}))
+  .plugin(bar({c: 'd'}))
+  .plugin(baz({e: 'f'}))
+
+
+console.log(app);
